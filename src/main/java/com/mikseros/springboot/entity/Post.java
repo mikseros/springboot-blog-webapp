fe9f93af75e3bba23dd635d5469121b61f935471
mikseros <mmadynski@gmail.com>
2022-09-11 01:09:45 +0200

    Post Entity created

--- src/main/java/com/mikseros/springboot/entity/Post.java
+++ src/main/java/com/mikseros/springboot/entity/Post.java
@@ -2,7 +2,15 @@ package com.mikseros.springboot.entity;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,11 +26,23 @@ import lombok.Setter;
 @Entity
 @Table(name = "posts")
 public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false)
 	private String title;
 	private String url;
+	
+	// @Lob annotation gives "tinytext" in the DB table. This is the reason why "columnDefinition" is used here.
+	@Column(nullable = false, columnDefinition = "longtext")
 	private String content;
 	private String shortDescription;
+	
+	@CreationTimestamp
 	private LocalDateTime createdOn;
+	
+	@UpdateTimestamp
 	private LocalDateTime updatedOn;
 }
