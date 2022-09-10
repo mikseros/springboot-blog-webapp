54b00dbf58b21cb8cca78c25e0ba7c40ef87f502
mikseros <mmadynski@gmail.com>
2022-09-10 23:23:49 +0200

    Initial commit

---
+++ src/main/java/com/mikseros/springboot/SpringbootBlogWebappApplication.java
@@ -0,0 +1,13 @@
+package com.mikseros.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogWebappApplication.class, args);
+	}
+
+}
