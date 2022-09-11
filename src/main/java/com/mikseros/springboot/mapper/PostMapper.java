b4e314b269bffa7c970fe548459f90207beccb1b
mikseros <mmadynski@gmail.com>
2022-09-11 19:30:38 +0200

    PostMapper created

---
+++ src/main/java/com/mikseros/springboot/mapper/PostMapper.java
@@ -0,0 +1,34 @@
+// This class is for mapping PostDto to Post & vice-versa.
+package com.mikseros.springboot.mapper;
+
+import com.mikseros.springboot.dto.PostDto;
+import com.mikseros.springboot.entity.Post;
+
+public class PostMapper {
+	
+	// Map Post Entity to PostDto
+	public static PostDto mapToPostDto(Post post) {
+		return PostDto.builder()
+				.id(post.getId())
+				.title(post.getTitle())
+				.url(post.getUrl())
+				.content(post.getContent())
+				.shortDescription(post.getShortDescription())
+				.createdOn(post.getCreatedOn())
+				.updatedOn(post.getUpdatedOn())
+				.build();
+	}
+	
+	// Map PostDto to Post Entity
+	public static Post mapToPost(PostDto postDto) {
+		return Post.builder()
+				.id(postDto.getId())
+				.title(postDto.getTitle())
+				.url(postDto.getUrl())
+				.content(postDto.getContent())
+				.shortDescription(postDto.getShortDescription())
+				.createdOn(postDto.getCreatedOn())
+				.updatedOn(postDto.getUpdatedOn())
+				.build();
+	}
+}
