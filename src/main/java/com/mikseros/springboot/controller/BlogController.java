afcc3657c604cfdcaad2bd52080cfd15711297f6
mikseros <mmadynski@gmail.com>
2022-09-22 20:26:25 +0200

    View Blog Post implemented

--- src/main/java/com/mikseros/springboot/controller/BlogController.java
+++ src/main/java/com/mikseros/springboot/controller/BlogController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.mikseros.springboot.dto.PostDto;
 import com.mikseros.springboot.service.PostService;
@@ -24,6 +25,15 @@ public class BlogController {
 		List<PostDto> postsResponse = postService.findAllPosts();
 		model.addAttribute("postsResponse", postsResponse);
 		return "blog/view_posts";
+	}
+	
+	// handler method to handle view post request
+	@GetMapping("/post/{postUrl}")
+	private String showPost(@PathVariable("postUrl") String postUrl,
+							Model model) {
+		PostDto post = postService.findPostByUrl(postUrl);
+		model.addAttribute("post", post);
+		return "blog/blog_post";
 	}
 	
 }
