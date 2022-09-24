1230999aa546e968b6da10edaee3c712fec25a64
mikseros <mmadynski@gmail.com>
2022-09-24 20:18:40 +0200

    Create Comment Form Validation added

--- src/main/java/com/mikseros/springboot/controller/CommentController.java
+++ src/main/java/com/mikseros/springboot/controller/CommentController.java
@@ -2,27 +2,43 @@ package com.mikseros.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mikseros.springboot.dto.CommentDto;
+import com.mikseros.springboot.dto.PostDto;
 import com.mikseros.springboot.service.CommentService;
+import com.mikseros.springboot.service.PostService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class CommentController {
 
 	private CommentService commentService;
+	private PostService postService;
 
-	public CommentController(CommentService commentService) {
+	public CommentController(CommentService commentService, PostService postService) {
 		this.commentService = commentService;
+		this.postService = postService;
 	}
 	
 	// handler method to create form submit request
 	@PostMapping("/{postUrl}/comments")
 	public String createComment(@PathVariable("postUrl") String postUrl,
-								@ModelAttribute("comment") CommentDto commentDto,
+								@Valid @ModelAttribute("comment") CommentDto commentDto,
+								BindingResult result,
 								Model model) {
+		
+		PostDto postDto = postService.findPostByUrl(postUrl);
+		if(result.hasErrors()) {
+			model.addAttribute("post" ,postDto);
+			model.addAttribute("comment", commentDto);
+			return "blog/blog_post";
+		}
+		
 		commentService.createComment(postUrl, commentDto);
 		return "redirect:/post/" + postUrl;
 	}
--- src/main/java/com/mikseros/springboot/dto/CommentDto.java
+++ src/main/java/com/mikseros/springboot/dto/CommentDto.java
@@ -2,6 +2,8 @@ package com.mikseros.springboot.dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,8 +17,12 @@ import lombok.Setter;
 @Builder
 public class CommentDto {
 	private Long id;
+	@NotEmpty
 	private String name;
+	@NotEmpty(message = "Email should not be empty!")
+	@Email
 	private String email;
+	@NotEmpty(message = "Message body should not be empty!")
 	private String content;
 	private LocalDateTime createdOn;
 	private LocalDateTime updatedOn;
