74b848c6bf76629c7ef26beb20ed96a03c12249d
mikseros <mmadynski@gmail.com>
2022-10-02 21:27:34 +0200

    Database Authentication implemented

--- src/main/java/com/mikseros/springboot/config/WebSpringSecurity.java
+++ src/main/java/com/mikseros/springboot/config/WebSpringSecurity.java
@@ -1,9 +1,14 @@
 package com.mikseros.springboot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -11,6 +16,17 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebSpringSecurity {
 	
+	private UserDetailsService userDetailsService;
+	
+	public WebSpringSecurity(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+	
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable()
@@ -31,5 +47,11 @@ public class WebSpringSecurity {
 				);
 		return http.build();
 							
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		builder.userDetailsService(userDetailsService)
+				.passwordEncoder(passwordEncoder());
 	}
 }
--- src/main/java/com/mikseros/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/mikseros/springboot/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.mikseros.springboot.service.impl;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mikseros.springboot.dto.RegistrationDto;
@@ -17,10 +18,12 @@ public class UserServiceImpl implements UserService {
 
 	private UserRepository userRepository;
 	private RoleRepository roleRepository;
+	private PasswordEncoder passwordEncoder;
 	
-	public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+	public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
 		this.roleRepository = roleRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
@@ -29,7 +32,7 @@ public class UserServiceImpl implements UserService {
 		user.setName(registrationDto.getFirstName() + " " + registrationDto.getLastName());
 		user.setEmail(registrationDto.getEmail());
 		// use spring security to encrypt the password
-		user.setPassword(registrationDto.getPassword());
+		user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
 		Role role = roleRepository.findByName("ROLE_GUEST");
 		user.setRoles(Arrays.asList(role));
 		userRepository.save(user);
