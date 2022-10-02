c4a70a3ff84ec9d5b68cc08070c27dc931aa3164
mikseros <mmadynski@gmail.com>
2022-10-02 13:25:50 +0200

    Logout Feature

--- src/main/java/com/mikseros/springboot/config/WebSpringSecurity.java
+++ src/main/java/com/mikseros/springboot/config/WebSpringSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +24,10 @@ public class WebSpringSecurity {
 						.defaultSuccessUrl("/admin/posts")
 						.loginProcessingUrl("/login")
 						.permitAll()
+				).logout(
+						logout -> logout
+								.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+								.permitAll()
 				);
 		return http.build();
 							
