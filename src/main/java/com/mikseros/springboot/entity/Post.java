1583f63091250770dcf68ba54d94d93266144dd8
mikseros <mmadynski@gmail.com>
2022-10-03 22:29:08 +0200

    Many to One Relationship Between Post & User

--- src/main/java/com/mikseros/springboot/entity/Post.java
+++ src/main/java/com/mikseros/springboot/entity/Post.java
@@ -13,6 +13,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -48,6 +50,10 @@ public class Post {
 	
 	@UpdateTimestamp
 	private LocalDateTime updatedOn;
+	
+	@ManyToOne
+	@JoinColumn(name = "created_by", nullable = false)
+	private User createdBy;
 	
 	@OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
 	private Set<Comment> comments = new HashSet<>();
